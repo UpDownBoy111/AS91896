26b0e78bf26c8d4136d4a83cc7b984e7c7132322
UpDownBoy111 <michael.stables0@gmail.com>
2026-07-02 10:55:52 +1200

    V10.0

---
+++ AS 91986 V10.0/modules/handler.py
@@ -0,0 +1,71 @@
+import os
+import json
+import csv
+from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog
+import config
+import math
+
+
+class FileHandler:
+    
+    @staticmethod
+    def export_history_to_csv(amount: int, coordinate: dict, parent_window = None):
+        number = 1
+        x = coordinate["x"]
+        y = coordinate["y"]
+        x_iter = iter(x)
+        y_iter = iter(y)
+        
+        def x_y_coords():
+            x_coord1 = next(x_iter)
+            x_coord2 = next(x_iter)
+            y_coord1 = next(y_iter)
+            y_coord2 = next(y_iter)
+            return x_coord1, x_coord2, y_coord1, y_coord2
+
+        def calc():
+            x_coord1, x_coord2, y_coord1, y_coord2 = x_y_coords()
+            distance = round(float(math.sqrt((x_coord2 - x_coord1)**2 + (y_coord2 - y_coord1)**2)), 2)
+            if x_coord1 == x_coord2 or y_coord1 == y_coord2:
+                gradient = 0
+            else:
+                gradient = round(float(y_coord2 - y_coord1)/ (x_coord2 - x_coord1), 4)
+            midpointx = round(float((x_coord1 + x_coord2)/2), 2)
+            midpointy= round(float((y_coord1 + y_coord2)/2), 2)
+            c = round(float(y_coord1 - round(float(gradient * x_coord1), 2)), 2)
+            return x_coord1, x_coord2, y_coord1, y_coord2, distance, gradient, midpointx, midpointy, c
+
+        default_filename = f"Calculation_History_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+        filename = filedialog.asksaveasfilename(
+            parent=parent_window,
+            initialfile=default_filename,
+            defaultextension=".csv",
+            filetypes=[("CSV Files", "*.csv"), ("All Files", "*.*")],
+            title="History"
+        )
+        if not filename:
+            return None
+        try:
+            with open(filename, "w", newline="", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerow([f"Calculation History"])
+                while int(amount) >= number:
+                    try:
+                        x_coord1, x_coord2, y_coord1, y_coord2, distance, gradient, midpointx, midpointy, c = calc()
+                        writer.writerow([f"Line Number", f"{number}"])
+                        writer.writerow([f"Coordinate One: ({x_coord1} {y_coord1})"])
+                        writer.writerow([f"Coordinate Two: ({x_coord2} {y_coord2})"])
+                        writer.writerow([])
+                        writer.writerow([f"Gradient: {gradient}"])
+                        writer.writerow([f"Distance: {distance}"])
+                        writer.writerow([f"Midpoint: ({midpointx} {midpointy})"])
+                        writer.writerow([f"Equation of the line: y = {gradient}x + {c}"])
+                        writer.writerow([])
+                        number += 1
+                    except Exception as e:
+                        raise Exception(f"Failed to export history: {str(e)}")
+            return filename
+        except Exception as e:
+            raise Exception(f"Failed to export history: {str(e)}")
